f52501676d2b176af8ad8658bce26a0864dd21b3
DESKTOP-M3HIP2O\dbwjd <dbwjd20111@naver.com>
2023-01-19 22:12:28 +0900

    서버 시작 준비

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 
 const app = express();
 
+app.get('/', function(req, res) {
+    res.send("안녕");
+});
 
 app.listen(3001, function(){
     console.log('연결되었습니다.');
