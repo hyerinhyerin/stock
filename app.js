b88a755ca36bd4459e0f679ed5efe35eef5a1e88
dbwjd <dbwjd20111@naver.com>
2023-01-28 01:31:17 +0900

    랜덤으로 주가 변동 기능 구현 초안(아직 상황에 따른 완벽 기능 구현 안됨)

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const session = require("express-session");
+const { sequelize } = require('./models');
 //const path = require("path");
 
 const app = express();
@@ -9,20 +10,20 @@ app.use(express.static(__dirname + "/"));
 // 세션(미들웨어)
 app.use(
     session({
-        secret : "loginData",
-        resave : false,
-        saveUninitialized : true,
+        secret: "loginData",
+        resave: false,
+        saveUninitialized: true,
         // store : new FileStore(),
     })
 );
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.send("안녕");
 });
 
 const stockprice = require("./route/stockprice");
 app.use('/stockprice', stockprice);
 
-app.listen(3001, function(){
+app.listen(3001, function () {
     console.log('연결되었습니다.');
 });
---
+++ route/random_price.js
@@ -0,0 +1,54 @@
+const Company = require('../models/Company');
+
+// 랜덤으로 모든 회사 주가가 변동되는 함수 반복 실행.
+function randomPrice() {
+    var randomNum = 0;
+    var randomOperation = "";
+
+    // 랜덤 숫자 내보내기(100~5000 사이 가격)
+    randomNum = Math.floor(Math.random() * (500 - 10) + 1);
+    randomNum = parseInt(randomNum + "0");
+
+    // 주가 변동(오르락 내리락 랜덤)
+    const operation = ["+", "-"];
+    randomOperation = operation[Math.floor(Math.random() * operation.length)];
+
+    var afterPrice = [];
+
+    Company.findAll({ attributes: ['stockprice'], raw: true })
+        .then((result) => {
+            for (let i in result) {
+                afterPrice.push(eval(result[i].stockprice + randomOperation + randomNum));
+            }
+            console.log(afterPrice);
+        })
+        .then(() => {
+            for (let i = 0; i < afterPrice.length; i++) {
+                Company.update({ stockprice: afterPrice[i] }, { where: { num: i + 1 } });
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+};
+
+var Ran;
+
+function start() {
+    console.log("시작");
+    Ran = setInterval(randomPrice, 1000);
+}
+
+function stop() {
+    console.log("멈춰");
+    clearInterval(Ran);
+}
+
+
+module.exports.startRandom = function () {
+    start();
+}
+
+module.exports.stopRandom = function () {
+    stop();
+}
