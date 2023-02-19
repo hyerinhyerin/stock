5bb00a1cd55ef8287832edbf52a3cde72f7d6878
jherin <jhr6454@naver.com>
2023-02-19 16:08:30 +0900

    기본 구성

---
+++ react/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Mypage from './Mypage/Mypage';
+import LogoCP from './Component/LogoCP';
+import Start from './Login after/Start';
+import Join from './Join/Join';
+import './App.css';
+
+const App = () => {
+  return (
+  <div> 
+    <LogoCP/>
+    <Join/>
+  </div>
+  );
+};
+
+export default App;
--- react/src/index.js
+++ react/src/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Join from './Join/Join';
-import Mypage from './Mypage/Mypage'
-import LogoCP from './Component/LogoCP';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <LogoCP/>
-    <Mypage />
+    <App/>
   </React.StrictMode>
 );
 
