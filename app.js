28c603f2a899b5a63c0902bc7e2b5ac4dd506e3d
dbwjd <dbwjd20111@naver.com>
2023-03-20 14:11:02 +0900

    merge 중

--- app.js
+++ app.js
@@ -14,23 +14,23 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // 세션(미들웨어)
 app.use(
-    session({
-        secret: "loginData",
-        resave: false,
-        saveUninitialized: true,
-        // store : new FileStore(),
-    })
+  session({
+    secret: "loginData",
+    resave: false,
+    saveUninitialized: true,
+    // store : new FileStore(),
+  })
 );
 
 app.use(cp());
 //app.use(cors());
 
 app.get('/', function (req, res) {
-    res.send("완료");
+  res.send("완료");
 });
 
 app.get('/api/main', async (req, res) => {
-    res.send({ message: "안녕" });
+  res.send({ message: "안녕" });
 });
 
 const stockprice = require("./route/stockprice");
@@ -52,5 +52,5 @@ const buy = require("./route/buyStock");
 app.use('/buy', buy);
 
 app.listen(3001, function () {
-    console.log('연결되었습니다.');
+  console.log('연결되었습니다.');
 });
