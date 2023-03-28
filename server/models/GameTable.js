0e5ee9afe89e0673776da7fe3df2bbc5498599f9
dbwjd <dbwjd20111@naver.com>
2023-03-28 14:09:25 +0900

    매수 기능 구현 중

--- server/models/GameTable.js
+++ server/models/GameTable.js
@@ -4,30 +4,28 @@ module.exports = class Company extends Sequelize.Model {
     static init(sequelize) {
         return super.init(
             {
-                companyname: {
+                usernickname: {
                     type: Sequelize.STRING(45),
-                    allowNull: false,
-                },
-                companystock: {
-                    type: Sequelize.INTEGER,
-                    allowNull: false,
-                },
-                stockprice: {
-                    type: Sequelize.INTEGER,
-                    allowNull: false,
-                },
-                num: {
-                    type: Sequelize.INTEGER,
                     primaryKey: true,
-                    allowNull: false
+                    allowNull: false,
+                },
+                money: {
+                    type: Sequelize.INTEGER,
+                    allowNull: false,
+                },
+                havestock: {
+                    type: Sequelize.JSON,
+                },
+                created_at: {
+                    type: Sequelize.DATE,
                 },
             },
             {
                 sequelize,
                 timestamps: false,
                 underscored: false,
-                modelName: "Company", // 모델 이름을 설정, 노드 프로젝트에서 사용
-                tableName: "gamecompany",
+                modelName: "GameTable", // 모델 이름을 설정, 노드 프로젝트에서 사용
+                tableName: "gametable",
                 paranoid: false,
                 charset: "utf8", //한글을 입력하기 위한 설정
                 collate: "utf8_general_ci", //한글을 입력하기 위한 설정
--- server/models/index.js
+++ server/models/index.js
@@ -1,23 +1,38 @@
 const Sequelize = require("sequelize");
 
 const env = process.env.NODE_ENV || "development";
-const config = require("../config/config")[env];
+const config = require(__dirname + '/../config/config.json')[env];
+
 const User = require("./User");
+const Company = require("./Company");
+const Situation = require("./Situation");
+const UserFindPw = require("./UserFindPw");
+const GameTable = require("./GameTable");
 
 const sequelize = new Sequelize(
   config.database,
   config.username,
   config.password,
-  config
-);
+  config,
 
+);
 const db = {};
 
 db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
 db.User = User;
+db.UserFindPw = UserFindPw;
+db.Company = Company;
+db.Situation = Situation;
+db.GameTable = GameTable;
 
 User.init(sequelize);
+Company.init(sequelize);
+Situation.init(sequelize);
+UserFindPw.init(sequelize);
+GameTable.init(sequelize);
 
-// User.associate(db); // 관계 정의가 필요하면 사용
+// User.associate(db);
 
 module.exports = db;
