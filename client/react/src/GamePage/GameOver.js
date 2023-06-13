a05999d33268a7098eddeae83d14c827d72598d3
dbwjd <dbwjd20111@naver.com>
2023-06-14 01:42:49 +0900

    mainPage 오류 해결

--- client/react/src/GamePage/GameOver.js
+++ client/react/src/GamePage/GameOver.js
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
 import "./gameover.css";
 const GameOver = (props) => {
   axios.get("/api/gameOver");
